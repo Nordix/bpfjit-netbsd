26f3799229ced1e116b4aa6b98c17cf23f9faa2b
Bjorn Svensson <bjorn.a.svensson@est.tech>
2021-02-19 09:10:35 +0100

    Rewrite and patch bpfjit files during an update
    During an update the bpfjit files in this repo will first be
    replaced by a new merge from the netbsd repo, and then patched
    to be able to build with later versions of sljit.
    Replacing the files instead of merging them avoid he merge problems
    triggered by the required patch commit.

--- README.md
+++ README.md
@@ -7,12 +7,10 @@ Manpage: https://man.netbsd.org/bpfjit.4
 bpfjit depends on sljit: https://github.com/zherczeg/sljit/
 
 This repo contains the bpfjit files and their history extracted from the NetBSD
-repo at https://github.com/NetBSD/src. See the script `update.sh` for details
-about how their history is extracted.
+repo at https://github.com/NetBSD/src. The files are patched to be able
+to build using the latest sljit. See the script `update.sh` for details
+about code changes and how the file history is extracted.
 
 TODO:
 
 - Add Makefile.
-
-- Make sure bpfjit can be built using the latest sljit. (Add minimal patch if
-  necessary.)
--- update.sh
+++ update.sh
@@ -61,7 +61,7 @@ fi
 # Forced rewrite needed when using a restored netbsd repo.
 cd netbsd-src
 $gfr --path sys/net/bpf.h --path sys/net/bpfjit.c --path sys/net/bpfjit.h \
-     --path-rename sys/net/:src/ \
+     --path-rename sys/:src/ \
      --force \
      || die "Failed to rewrite history using git-filter-repo"
 
@@ -70,14 +70,46 @@ cd $reporoot
 
 # Make a backup of our ".git"
 tar -cjf "_update/gitbackup$(date +%Y-%m-%d).tar.bz2" .git
-rm -rf .git src \
-    && mv _update/netbsd-src/.git _update/netbsd-src/src . \
-    && rmdir _update/netbsd-src \
-        || die "Failed to replace our repo with the filtered netbsd-src repo"
 
-# Add this script, README, etc. as a commit on top of everything.
-git add src README.md update.sh .gitignore \
-    && git commit -m "Update bpfjit from NetBSD" \
-        || die "Failed to commit update"
+# Remove current bpfjit files from our own repo.
+# TODO: Remove --force flag, but this requires a fresh clone
+$gfr --invert-paths \
+     --path src/net/bpf.h --path src/net/bpfjit.c --path src/net/bpfjit.h \
+     --force \
+    || die "Failed to rewrite history using git-filter-repo"
+
+# Merge latest bpfjit files to our own repo.
+git remote remove netbsd
+git remote add netbsd _update/netbsd-src \
+    || die "Failed to add 'netbsd-src' remote repo"
+git fetch netbsd --tags \
+    || die "Failed to fetch remote repo 'netbsd-src'"
+git merge -m "Merge bpfjit from NetBSD" \
+    --allow-unrelated-histories netbsd/trunk \
+    || die "Failed to merge remote repo 'netbsd-src'"
+git remote remove netbsd \
+    || die "Failed to remove 'netbsd-src' remote repo"
+
+# Patch bpfjit files to compile.
+# Remove unused includes
+sed -i 's|\(#include <net/dlt.h>\)|/* \1 */|g' src/net/bpf.h
+sed -i 's|\(#include <sys/ioccom.h>\)|/* \1 */|g' src/net/bpf.h
+sed -i 's|\(#include <pcap-bpf.h>\)|/* \1 */|g' src/net/bpfjit.h
+# Remove assert using unknown macro
+sed -i 's|\(__CTASSERT.*\)$|/* \1 */|g' src/net/bpf.h
+# Ignore unknown macro __RCSID
+sed -i 's|^__RCSID|#define __RCSID(s)\n__RCSID|g' src/net/bpfjit.c
+# Updates needed for latest version of sljit
+sed -i 's/sljit_emit_ijump/sljit_emit_icall/g' src/net/bpfjit.c
+sed -i 's/SLJIT_CALL2,/SLJIT_CALL, SLJIT_ARG1(SW) | SLJIT_ARG2(SW),/g' src/net/bpfjit.c
+sed -i 's/SLJIT_CALL3,/SLJIT_CALL, SLJIT_ARG1(SW) | SLJIT_ARG2(SW) | SLJIT_ARG3(SW),/g' src/net/bpfjit.c
+sed -i 's/sljit_create_compiler(NULL)/sljit_create_compiler(NULL, NULL)/g' src/net/bpfjit.c
+sed -i 's/sljit_emit_enter(compiler, 0, 2,/sljit_emit_enter(compiler, 0, SLJIT_ARG1(SW) | SLJIT_ARG2(SW),/g' src/net/bpfjit.c
+sed -i 's/sljit_free_code((void \*)code)/sljit_free_code((void *)code, NULL)/g' src/net/bpfjit.c
+
+# Commit patched files.
+git add src/net/bpf.h src/net/bpfjit.h src/net/bpfjit.c \
+    && git commit -m "Patch bpfjit to build with latest sljit" \
+        || die "Failed to commit patch"
 
 echo "Done."
